71fe243cd1cac4369c6388a4024a679f75ae9797
Lea Simone <apple@MacBookPro.attlocal.net>
2025-03-25 08:24:41 -0500

    removed controllers for now

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,48 +1,48 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
 
-const register = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+// const register = async (req, res) => {
+//     try {
+//         const { username, email, password } = req.body;
 
-        // Check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) return res.status(400).json({ message: "User already exists" });
+//         // Check if user already exists
+//         const existingUser = await User.findOne({ email });
+//         if (existingUser) return res.status(400).json({ message: "User already exists" });
 
-        // Hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+//         // Hash password
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // Create new user
-        const newUser = new User({ username, email, password: hashedPassword });
-        await newUser.save();
+//         // Create new user
+//         const newUser = new User({ username, email, password: hashedPassword });
+//         await newUser.save();
 
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
-};
+//         res.status(201).json({ message: "User registered successfully" });
+//     } catch (err) {
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
 
-const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+// const login = async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
 
-        // Check if user exists
-        const user = await User.findOne({ email });
-        if (!user) return res.status(404).json({ message: "User not found" });
+//         // Check if user exists
+//         const user = await User.findOne({ email });
+//         if (!user) return res.status(404).json({ message: "User not found" });
 
-        // Compare passwords
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+//         // Compare passwords
+//         const isMatch = await bcrypt.compare(password, user.password);
+//         if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
-        // Generate JWT token
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+//         // Generate JWT token
+//         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-        res.json({ token, user });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
-};
+//         res.json({ token, user });
+//     } catch (err) {
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
 
-module.exports = { register, login };
+// module.exports = { register, login };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,53 +1,53 @@
-const User = require("../models/User");
+//const User = require("../models/User");
 
-const getUserProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id).select("-password");
-        if (!user) return res.status(404).json({ message: "User not found" });
+//const getUserProfile = async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id).select("-password");
+//         if (!user) return res.status(404).json({ message: "User not found" });
 
-        res.json(user);
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
-};
+//         res.json(user);
+//     } catch (err) {
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
 
-const followUser = async (req, res) => {
-    try {
-        const userToFollow = await User.findById(req.params.id);
-        const currentUser = await User.findById(req.user.id);
+// const followUser = async (req, res) => {
+//     try {
+//         const userToFollow = await User.findById(req.params.id);
+//         const currentUser = await User.findById(req.user.id);
 
-        if (!userToFollow || !currentUser) return res.status(404).json({ message: "User not found" });
+//         if (!userToFollow || !currentUser) return res.status(404).json({ message: "User not found" });
 
-        if (!currentUser.following.includes(userToFollow._id)) {
-            currentUser.following.push(userToFollow._id);
-            userToFollow.followers.push(currentUser._id);
-            await currentUser.save();
-            await userToFollow.save();
-        }
+//         if (!currentUser.following.includes(userToFollow._id)) {
+//             currentUser.following.push(userToFollow._id);
+//             userToFollow.followers.push(currentUser._id);
+//             await currentUser.save();
+//             await userToFollow.save();
+//         }
 
-        res.json({ message: "User followed" });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
-};
+//         res.json({ message: "User followed" });
+//     } catch (err) {
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
 
-const unfollowUser = async (req, res) => {
-    try {
-        const userToUnfollow = await User.findById(req.params.id);
-        const currentUser = await User.findById(req.user.id);
+// const unfollowUser = async (req, res) => {
+//     try {
+//         const userToUnfollow = await User.findById(req.params.id);
+//         const currentUser = await User.findById(req.user.id);
 
-        if (!userToUnfollow || !currentUser) return res.status(404).json({ message: "User not found" });
+//         if (!userToUnfollow || !currentUser) return res.status(404).json({ message: "User not found" });
 
-        currentUser.following = currentUser.following.filter(id => id.toString() !== userToUnfollow._id.toString());
-        userToUnfollow.followers = userToUnfollow.followers.filter(id => id.toString() !== currentUser._id.toString());
+//         currentUser.following = currentUser.following.filter(id => id.toString() !== userToUnfollow._id.toString());
+//         userToUnfollow.followers = userToUnfollow.followers.filter(id => id.toString() !== currentUser._id.toString());
 
-        await currentUser.save();
-        await userToUnfollow.save();
+//         await currentUser.save();
+//         await userToUnfollow.save();
 
-        res.json({ message: "User unfollowed" });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
-};
+//         res.json({ message: "User unfollowed" });
+//     } catch (err) {
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
 
-module.exports = { getUserProfile, followUser, unfollowUser };
+// module.exports = { getUserProfile, followUser, unfollowUser };
